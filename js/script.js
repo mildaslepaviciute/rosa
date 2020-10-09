92cb02c723ed1a315442ce1f54be5507b41db90a
mildaslepaviciute <milda.slepaviciute@gmail.com>
2020-10-09 13:10:45 +0300

    c

--- js/script.js
+++ js/script.js
@@ -2,3 +2,47 @@
 
 $('#year').text(new Date().getFullYear());
 
+
+
+
+$(document).ready(function() {
+    $('.js--sticky-navbar-offset').waypoint(function(direction) {
+        if (direction == "down") {
+            $('#navbar-sticky').removeClass('d-md-none');
+            $('#navbar-sticky').addClass('fixed-top');
+        } else {
+            $('#navbar-sticky').removeClass('fixed-top');
+            $('#navbar-sticky').addClass('d-md-none');
+        }
+    }, {
+        offset: '60px;'
+    });
+});
+
+
+
+
+// $("#gallery1").click(function(){
+//     $(".gallery-slider").removeClass("d-block");
+//     $(".gallery-slider").addClass("d-none");
+//     $("#gallery-slider1").removeClass("d-none");
+//     $("#gallery-slider1").addClass("d-block");
+// });
+
+// $("#gallery2").click(function(){
+//     $(".gallery-slider").removeClass("d-block");
+//     $(".gallery-slider").addClass("d-none");
+//     $("#gallery-slider2").removeClass("d-none");
+//     $("#gallery-slider2").addClass("d-block");
+// });
+
+// $("#gallery3").click(function(){
+//     $(".gallery-slider").removeClass("d-block");
+//     $(".gallery-slider").addClass("d-none");
+//     $("#gallery-slider3").removeClass("d-none");
+//     $("#gallery-slider3").addClass("d-block");
+// });
+
+
+
+  
