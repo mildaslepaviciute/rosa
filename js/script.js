27cc841ebf082346ee9ea9175d1d9ea23588d662
mildaslepaviciute <milda.slepaviciute@gmail.com>
2020-10-11 19:48:13 +0300

    c

--- js/script.js
+++ js/script.js
@@ -21,6 +21,6 @@ $(document).ready(function() {
 });
 
 
-$('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
-});
+// $('.navbar-nav>li>a').on('click', function(){
+//     $('.navbar-collapse').collapse('hide');
+// });
